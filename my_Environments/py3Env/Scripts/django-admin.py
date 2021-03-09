12c485fe9326fe6eb8b44730ef26d7c0372eddae
Luis Huertas <lc.huertas92@gmail.com>
2021-03-08 22:27:32 -0500

    homework

---
+++ my_Environments/py3Env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\luise\onedrive\desktop\codingdojo\python_stack\codingdojo_python_stack\my_environments\py3env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
