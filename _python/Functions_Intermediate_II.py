fd867183aa0811644c4889e057f001c1b893f252
Luis Huertas <77518276+lc-huertas@users.noreply.github.com>
2021-03-14 18:33:39 -0400

    Update Functions_Intermediate_II.py

--- _python/Functions_Intermediate_II.py
+++ _python/Functions_Intermediate_II.py
@@ -22,6 +22,85 @@ print(sports_directory)
 #
 z = [ {'x': 10, 'y': 20} ]
 # Change the value 20 in z to 30
-z['y']=30
-#
+z[0]['y']=30
+print(z)
+#=====================================================================
 
+# Create a function iterateDictionary(some_list) that, given a list of dictionaries,
+# the function loops through each dictionary in the list and prints each key and the
+# associated value. For example, given the following list:
+
+students = [
+         {'first_name':  'Michael', 'last_name' : 'Jordan'},
+         {'first_name' : 'John', 'last_name' : 'Rosales'},
+         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+
+def iterateDictionary(data):
+    for person in data:
+        print(f"first_name - {person['first_name']}, last_name - {person['last_name']}")
+
+iterateDictionary(students)
+# should output: (it's okay if each key-value pair ends up on 2 separate lines;
+# bonus to get them to appear exactly as below!)
+# first_name - Michael, last_name - Jordan
+# first_name - John, last_name - Rosales
+# first_name - Mark, last_name - Guillen
+# first_name - KB, last_name - Tonel
+
+# Create a function iterateDictionary2(key_name, some_list) that, given a list of dictionaries
+# and a key name, the function prints the value stored in that key for each dictionary.
+# For example, iterateDictionary2('first_name', students) should output:
+#   Michael
+#   John
+#   Mark
+#   KB
+
+def iterateDictionary2(key,data):
+    for person in data:
+        print(person[key])
+
+iterateDictionary2('first_name',students)
+iterateDictionary2('last_name',students)
+
+# Create a function printInfo(some_dict) that given a dictionary whose values are all lists,
+# prints the name of each key along with the size of its list, and then prints the associated
+# values within each key's list. For example:
+
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+def printInfo(data):
+    for set in dojo:
+        num = len(data[set])
+        [print(f"{num} {set}")]
+        for content in data[set]:
+            print(content)
+
+printInfo(dojo)
+
+# output:
+# 7
+# LOCATIONS
+# San
+# Jose
+# Seattle
+# Dallas
+# Chicago
+# Tulsa
+# DC
+# Burbank
+
+# 8
+# INSTRUCTORS
+# Michael
+# Amy
+# Eduardo
+# Josh
+# Graham
+# Patrick
+# Minh
+# Devon
