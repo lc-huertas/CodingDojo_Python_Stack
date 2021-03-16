4fb9411ea48786ec2c1c39382577ba2c86a3fe90
Luis Huertas <lc.huertas92@gmail.com>
2021-03-16 18:56:31 -0400

    Update User.py

--- _python/python_fundamentals/User.py
+++ _python/python_fundamentals/User.py
@@ -7,11 +7,14 @@ class User:  # declare a class and give it name User
     def deposit(self, amount):
         self.account_balance += amount
         print(f"{self.name} deposited ${amount}")
+        return self
     def withdrawal(self, amount):
         self.account_balance -= amount
         print(f"{self.name} withdrew ${amount}")
+        return self
     def balance(self):
         print(f"{self.name} -> balance: ${self.account_balance}")
+        return self
 
     def transfer(self, recipient, amount):
         if (self.account_balance - amount < 0):
@@ -20,19 +23,13 @@ class User:  # declare a class and give it name User
             self.withdrawal(amount)
             recipient.deposit(amount)
             print(f"{self.name} transferred ${amount} to {recipient.name}")
+        return self
 
 
 My = User("Luis Huertas", "lhuertas92@gmail.com")
 print(My.email)
 
-My.deposit(50)
-My.balance()
-My.withdrawal(5)
-My.balance()
+My.deposit(50).withdrawal(5).balance()
 
 President = User("Joe Biden", "sleepyjoe46@aol.com")
-President.deposit(1e5)
-President.transfer(My, 1400)
-My.balance()
-My.withdrawal(1200)
-My.balance()
+President.deposit(1e5).transfer(My, 1400)
